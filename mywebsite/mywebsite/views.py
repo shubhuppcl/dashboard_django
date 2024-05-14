9895e1a7af45bb86bb4e9e6e3659c0d925e6ce58
shubhuppcl <164376936+shubhuppcl@users.noreply.github.com>
2024-05-14 16:19:08 +0530

    Add files via upload

---
+++ mywebsite/mywebsite/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+
+
+def home(request):
+    return render(request, 'registration/home.html')  # Assuming you have a template named 'home.html'
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+    return render(request, 'registration/login.html')
+
+def dashboard_view(request):
+    return render(request, 'registration/dashboard.html')
